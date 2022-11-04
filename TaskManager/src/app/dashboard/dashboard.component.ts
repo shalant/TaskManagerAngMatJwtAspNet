3f44afb56d9e5b77500c118430ea5a2889f67f55
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-04 11:05:04 -0500

    working on dashboard

--- TaskManager/src/app/dashboard/dashboard.component.ts
+++ TaskManager/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  Designation: string = "blah";
+  Username: string = "blah";
+  NoOfTeamMembers: number = 108;
+  TotalCostOfAllProjects: number = 108;
+  PendingTasks: number= 108;
+  UpComingProjects: number= 108;
+  ProjectCost: number= 108;
+
+  CurrentExpenditure: number= 108;
+  AvailableFunds: number= 108;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.Designation = "Team Leader";
+    this.Username = "John Smith";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects= 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 52536;
+    this.AvailableFunds = 52536;
   }
 
 }
