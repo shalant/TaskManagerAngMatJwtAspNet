8c505341873222162f7a0dd2cae1b7f6885255c1
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-04 15:32:50 -0500

    really complex form displaying many fields, including nested ngFor

--- TaskManager/src/app/dashboard/dashboard.component.ts
+++ TaskManager/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { exhaustAll } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,16 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  Designation: string = "blah";
-  Username: string = "blah";
-  NoOfTeamMembers: number = 108;
-  TotalCostOfAllProjects: number = 108;
-  PendingTasks: number= 108;
-  UpComingProjects: number= 108;
-  ProjectCost: number= 108;
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number= 0;
+  UpComingProjects: number= 0;
+  ProjectCost: number= 0;
 
-  CurrentExpenditure: number= 108;
-  AvailableFunds: number= 108;
+  CurrentExpenditure: number= 0;
+  AvailableFunds: number= 0;
+
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers:any = [];
 
   constructor() { }
 
@@ -30,6 +37,53 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 52536;
     this.AvailableFunds = 52536;
+
+    this.Clients = [
+      "ABC Infotech Ltd.", "DEF Software", "GHI Industries"
+    ]
+
+    this.Projects = [
+      "Project A", "Project B", "Project C", "Project D"
+    ]
+
+    for (var i =2019; i >= 2010; i--)
+    {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      { Region: "East", TeamMembersCount: 10, TemporaryUnavailableMembers: 1},
+      { Region: "West", TeamMembersCount: 20, TemporaryUnavailableMembers: 4},
+      { Region: "South", TeamMembersCount: 14, TemporaryUnavailableMembers: 2},
+      { Region: "North", TeamMembersCount: 3, TemporaryUnavailableMembers: 4},
+    ]
+
+    this.TeamMembers = [
+      { Region: "East", Members: [
+        { ID: 1, Name: "Ford", Status: "Available"},
+        { ID: 2, Name: "Miller", Status: "Available"},
+        { ID: 3, Name: "Jones", Status: "Busy"},
+        { ID: 4, Name: "James", Status: "Busy"},
+      ]},
+      { Region: "West", Members: [
+        { ID: 1, Name: "Ford", Status: "Available"},
+        { ID: 2, Name: "Miller", Status: "Available"},
+        { ID: 3, Name: "Jones", Status: "Busy"},
+        { ID: 4, Name: "James", Status: "Busy"},
+      ]},
+      { Region: "South", Members: [
+        { ID: 1, Name: "Ford", Status: "Available"},
+        { ID: 2, Name: "Miller", Status: "Available"},
+        { ID: 3, Name: "Jones", Status: "Busy"},
+        { ID: 4, Name: "James", Status: "Busy"},
+      ]},
+      { Region: "North", Members: [
+        { ID: 1, Name: "Ford", Status: "Available"},
+        { ID: 2, Name: "Miller", Status: "Available"},
+        { ID: 3, Name: "Jones", Status: "Busy"},
+        { ID: 4, Name: "James", Status: "Busy"},
+      ]},
+    ]
   }
 
 }
